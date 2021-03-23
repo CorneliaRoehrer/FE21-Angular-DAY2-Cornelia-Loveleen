0953fd4614127720589bee5c9de46c8e1b58f505
CorneliaRoehrer <corne.lia.roehrer@gmail.com>
2021-03-23 14:50:30 +0100

    add contact form

--- carRental/src/app/contact-us/contact-us.component.ts
+++ carRental/src/app/contact-us/contact-us.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent implements OnInit {
 
+export class ContactUsComponent implements OnInit {
+  contactForm = new FormGroup({
+    firstName: new FormControl("",Validators.required),
+    lastName: new FormControl("",Validators.required),
+    age: new FormControl("",Validators.required),
+    driverLicenseNumber: new FormControl("",Validators.required),
+    eMailAdress: new FormControl("",Validators.required),
+  });
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(){
+    if(this.contactForm.valid){
+    var a = this.contactForm.value;
+    console.log(a)
+    }
+  }
 }
